b544e5bca7bc8cda81f480016b56f28c2c9c506c
ChampX5 <oshal.baranwal@gmail.com>
2025-09-14 13:13:01 +0530

    feature: added recipe filtering and UI for the same

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const getIngredientNamesAndIDs = (ingredientsData) =>
     });
 
 const updateLocalStorage = (updatedIngredients, ingredientNamesAndIDs) => {
-    if (updatedIngredients.length == 0) {
+    if (updatedIngredients.length === 0) {
         localStorage.clear();
         updatePantryIngredients(updatedIngredients, ingredientNamesAndIDs);
         return;
@@ -35,26 +35,45 @@ const readLocalStorage = () => {
 };
 
 const getImageURL = (name) => {
-    name = name.toLowerCase();
-    name = name.replace(/ /g, '_');
+    return `https://www.themealdb.com/images/ingredients/${getURLName(
+        name
+    )}.png`;
+};
 
-    return `https://www.themealdb.com/images/ingredients/${name}.png`;
+const getURLName = (name) => {
+    name = name.toLowerCase();
+    return name.replace(/ /g, '_');
 };
 
 const updatePantryIngredients = (ingredientIDs, ingredientNamesAndIDs) => {
     const pantryElementsContainer = document.querySelector('div.pantry-items');
     pantryElementsContainer.replaceChildren([]);
 
+    const parentElement = pantryElementsContainer.parentElement;
+
     // dynamic spacing between searchbar and items
-    const searchBarContainer = document.querySelector('div:has(input#pantry-input[type="text"])');
+    const searchBarContainer = document.querySelector(
+        'div:has(input#pantry-input[type="text"])'
+    );
     searchBarContainer.classList.remove('mb-5');
+    pantryElementsContainer.classList.remove('mb-5');
+
+    if (parentElement.children.length === 4) {
+        parentElement.removeChild(parentElement.children.item(3));
+    }
 
     if (ingredientIDs.length > 0) {
         searchBarContainer.classList.add('mb-5');
+        pantryElementsContainer.classList.add('mb-5');
+    } else {
+        updateRecipes([]);
+        return;
     }
 
     for (let i = 0; i < ingredientIDs.length; i++) {
-        const ingredient = ingredientNamesAndIDs.find((elem) => elem.id === ingredientIDs[i]);
+        const ingredient = ingredientNamesAndIDs.find(
+            (elem) => elem.id === ingredientIDs[i]
+        );
 
         const mainElement = document.createElement('div');
         mainElement.classList.add('pantry-item');
@@ -83,7 +102,7 @@ const updatePantryIngredients = (ingredientIDs, ingredientNamesAndIDs) => {
         svgCancelElement.addEventListener('click', () => {
             // since JS doesn't have a `.remove` function or similar, we are going to have to maneuver around it
             const temp = [];
-            
+
             while (ingredientIDs.length > i + 1) {
                 temp.push(ingredientIDs.pop());
             }
@@ -103,6 +122,98 @@ const updatePantryIngredients = (ingredientIDs, ingredientNamesAndIDs) => {
         mainElement.appendChild(svgCancelElement);
 
         pantryElementsContainer.appendChild(mainElement);
+    }
+
+    // add a button that filters recipes and shows
+    const button = document.createElement('button');
+    button.className =
+        'bg-gray-700 h-20 w-full justify-center items-center flex cursor-pointer hover:bg-green-300';
+    button.innerHTML = `
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            class="fill-green-500"
+        >
+            <path
+                d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"
+            />
+        </svg>
+    `;
+
+    parentElement.appendChild(button);
+    button.addEventListener('click', async () => {
+        const recipes = [];
+        const recipeIDs = [];
+
+        for (const id of ingredientIDs) {
+            const ingredient = ingredientNamesAndIDs.find(
+                (elem) => elem.id === id
+            );
+
+            const res = await fetch(
+                `https://www.themealdb.com/api/json/v1/1/filter.php?i=${getURLName(
+                    ingredient.name
+                )}`
+            );
+            const data = await res.json();
+
+            const meals = data.meals;
+
+            for (const meal of meals) {
+                if (meal.idMeal in recipeIDs) continue;
+
+                recipeIDs.push(meal.idMeal);
+                recipes.push(meal);
+            }
+        }
+
+        updateRecipes(recipes);
+    });
+};
+
+const updateRecipes = (recipes) => {
+    const container = document.querySelector('div.recipes-container');
+    container.replaceChildren([]);
+
+    for (const recipe of recipes) {
+        const name =
+            recipe.strMeal.length <= 25
+                ? recipe.strMeal
+                : recipe.strMeal.slice(0, 22) + '...';
+        const thumbnailURL = recipe.strMealThumb;
+        const id = recipe.idMeal;
+
+        const mainElement = document.createElement('div');
+        mainElement.classList.add('recipe');
+
+        const thumbnailDiv = document.createElement('div');
+        thumbnailDiv.classList.add('recipe-thumbnail');
+
+        const thumbnailImg = document.createElement('img');
+        thumbnailImg.src = thumbnailURL;
+
+        thumbnailDiv.appendChild(thumbnailImg);
+
+        const detailsDiv = document.createElement('div');
+        detailsDiv.classList.add('recipe-details');
+        detailsDiv.innerText = name;
+
+        detailsDiv.addEventListener('click', async () => {
+            const res = await fetch(
+                `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+            );
+            const data = await res.json();
+            const link = data.meals[0].strYoutube;
+
+            window.location = link;
+        });
+
+        mainElement.appendChild(thumbnailDiv);
+        mainElement.appendChild(detailsDiv);
+
+        container.appendChild(mainElement);
     }
 };
 
